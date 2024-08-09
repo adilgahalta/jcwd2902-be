2cc598623c9fa7128483541d1c90d86c57e68515
Jordan <74591126+jordansmrd@users.noreply.github.com>
2024-08-09 15:11:01 +0700

    day 1

--- day 1/express api/src/routers/user.route.ts
+++ day 1/express api/src/routers/user.route.ts
@@ -1,24 +1,59 @@
 /** @format */
-
 import { Router } from "express";
-
+import fs from "fs";
 export const userRouter = Router();
+const path = __dirname + "/../../data.json";
+const path2 = __dirname + "/../../log.txt";
 
+interface IUser {
+  id: number;
+  name: string;
+  age: number;
+}
+interface IData {
+  users: IUser[];
+}
+const getData = () => {
+  return JSON.parse(fs.readFileSync(path, "utf8"));
+};
+
+const writeData = (data: any) => {
+  fs.writeFileSync(path, JSON.stringify(data));
+};
+
+const writeLog = (message: string = "") => {
+  const log = fs.readFileSync(path2, "utf-8");
+  fs.writeFileSync(path2, log + " \n" + new Date() + " " + message);
+};
 userRouter.get("/", (req, res) => {
-  res.send("ini get user!");
+  //   const data = db.users;
+  const data = getData().users;
+
+  res.send({ success: true, data });
 });
 
 userRouter.post("/", (req, res) => {
-  res.send({
-    message: "ini post user!",
-    data: req.body,
+  const data = getData(); // { users: [] }
+  data.users.push(req.body); // { users: [ req.body ] }
+  fs.writeFileSync(path, JSON.stringify(data));
+  writeLog(JSON.stringify(req.body));
+  res.status(201).send({
+    success: true,
+    message: "new user has been added",
   });
 });
 
-userRouter.delete("/", (req, res) => {
+userRouter.delete("/:id", (req, res) => {
+  const data: IData = getData();
+  const index = data.users.findIndex((user) => user.id == Number(id));
+  data.users.splice(index, 1);
+  writeData(data);
   res.send("ini delete user!");
 });
 
-userRouter.patch("/", (req, res) => {
+userRouter.patch("/:id", (req, res) => {
   res.send("ini patch user!");
 });
+
+//crud
+// product => {id,product_name,desc}
