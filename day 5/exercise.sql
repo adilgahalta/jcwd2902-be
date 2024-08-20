4672c62fbd94a3da1c66a50e473643fc35eaf0fd
Jordan <74591126+jordansmrd@users.noreply.github.com>
2024-08-20 16:27:14 +0700

    day 7

--- day 5/exercise.sql
+++ day 5/exercise.sql
@@ -14,7 +14,8 @@ alter table students drop COLUMN gender;
 
 -- purwadhika branch
 use purwadhika_branch;
-create table branches (id int AUTO_INCREMENT PRIMARY KEY, branch_name VARCHAR(255), pic VARCHAR(255), address VARCHAR(255), city VARCHAR(255), province VARCHAR(255));
+create table branches (id int AUTO_INCREMENT PRIMARY KEY, branch_name VARCHAR(255) UNIQUE, pic VARCHAR(255), address VARCHAR(255), city VARCHAR(255), province VARCHAR(255));
 insert into branches (branch_name, pic, address, city, province) VALUES('BSD','THOMAS','GREEN OFFICE PARK 9', 'BSD', 'TANGERANG'), ('JKT','BUDI','MSIG TOWER', 'JAKARTA SELATAN', 'JAKARTA'), ('BTM', 'ANGEL','NONGSA', 'BATAM', 'KEP. RIAU');
+update branches set pic = 'DONO' where city = 'BSD';
 insert into branches (branch_name, pic, address, city, province) VALUES('BLI','TONO','GIANYAR','GIANYAR','BALI');
 select * from branches;
